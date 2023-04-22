07d9156bff7686394679ef672af575894bf07904
johypark97 <johypark97@gmail.com>
2023-04-22 14:44:35 +0900

    Update deploy scripts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const isProduction = process.env.NODE_ENV === 'production';
+
+const assetPrefix = isProduction ? '.' : '';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: assetPrefix,
+  env: { ASSET_PREFIX: assetPrefix },
+  images: { unoptimized: true },
+  output: 'export',
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
